2fc4b98aada572293ba1c936c5b99d7757e2f1b1
Ульяна <basalaeva.u.i>
2022-10-08 23:45:34 +0700

    one commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,65 @@
+
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAvg() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+        long actual = service.sum(sales) / 12;
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long maxMonth = service.maxSales(sales);
+
+        System.out.println(maxMonth);
+    }
+
+    @Test
+    public void shouldMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long minMonth = service.minSales(sales);
+
+        System.out.println(minMonth);
+    }
+
+    @Test
+    public void shouldCountBelowAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long count = service.countBelowAvg(sales);
+
+        System.out.println(count);
+    }
+
+    @Test
+    public void shouldCountAboveAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long count = service.countAboveAvg(sales);
+
+        System.out.println(count);
+    }
+}
+
+
